1e990c341dcdf2e9a1255c58d97f95f6541d54c9
Ignacio Moenen <ignacio.moenen@gmail.com>
2021-10-01 02:07:27 -0300

    JS, Links, style and responsive adjustment added

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// Tooltip Bootstrap 5.1.1
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+// Smooth Scrolling
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
