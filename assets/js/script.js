4272bf8757fefdae65784841a2857b98891a1f92
Ignacio Moenen <ignacio.moenen@gmail.com>
2021-10-02 15:47:15 -0300

    adding more Javascripts events

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,21 @@ $(document).ready(function(){
       }
     });
   });
+
+  //creando variables
+  const iconoViajes1 = document.getElementById("iconoViajes1");
+  const iconoViajes2 = document.getElementById("iconoViajes2");
+  const iconoViajes3 = document.getElementById("iconoViajes3");
+
+  //Agregando estilo a la variable creada por evento click
+  iconoViajes1.addEventListener("click",function () {
+    iconoViajes1.classList.add("iconoRojo");
+});
+
+iconoViajes2.addEventListener("click",function () {
+  iconoViajes2.classList.add("iconoRojo");
+});
+
+iconoViajes3.addEventListener("click",function () {
+  iconoViajes3.classList.add("iconoRojo");
+});
